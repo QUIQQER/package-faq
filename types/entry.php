21b4655f81577b56e5732b4b6c7b195d0016f907
Henning Leutz <leutz@pcsg.de>
2015-03-27 16:20:01 +0100

    faq eintrag -> weiterleitung auf parent

--- types/entry.php
+++ types/entry.php
@@ -3,3 +3,17 @@
 /**
  * FAQ Entry
  */
+
+$Parent     = $Site->getParent();
+$parentType = $Parent->getAttribute( 'type' );
+
+if ( $parentType == 'quiqqer/faq:types/list' ||
+     $parentType == 'quiqqer/faq:types/category' )
+{
+    QUI::getRewrite()->showErrorHeader(
+        303,
+        URL_DIR . $Parent->getUrlRewrited() .'#faq'. $Site->getId()
+    );
+
+    exit;
+}
