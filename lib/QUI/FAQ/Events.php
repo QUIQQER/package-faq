ba4d8312014982afe6cf5239e028cc8649d21ac7
Henning Leutz <leutz@pcsg.de>
2015-03-27 14:54:17 +0100

    dev-dev

---
+++ lib/QUI/FAQ/Events.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file contains \QUI\FAQ\Events
+ */
+
+namespace QUI\FAQ;
+
+/**
+ * FAQ Events
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ */
+
+class Events
+{
+    /**
+     * event on child create
+     *
+     * @param Integer $newId
+     * @param \QUI\Projects\Site\Edit $Parent
+     */
+    static function onChildCreate($newId, $Parent)
+    {
+        $type = $Parent->getAttribute( 'type' );
+
+        if ( $type != 'quiqqer/faq:types/list' && $type != 'quiqqer/faq:types/category' ) {
+            return;
+        }
+
+        $Project = $Parent->getProject();
+        $Site    = new \QUI\Projects\Site\Edit( $Project, $newId );
+
+        if ( $type == 'quiqqer/faq:types/list' ) {
+            $Site->setAttribute( 'type', 'quiqqer/faq:types/category' );
+        }
+
+        if ( $type == 'quiqqer/faq:types/category' ) {
+            $Site->setAttribute( 'type', 'quiqqer/faq:types/entry' );
+        }
+
+        $Site->save();
+    }
+}
