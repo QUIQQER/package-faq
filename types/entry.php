73dd9d2f0a9030909fdda41b59bdc6dad6616561
Henning Leutz <leutz@pcsg.de>
2015-04-18 17:33:04 +0200

    psr-2

--- types/entry.php
+++ types/entry.php
@@ -4,15 +4,15 @@
  * FAQ Entry
  */
 
-$Parent     = $Site->getParent();
-$parentType = $Parent->getAttribute( 'type' );
+$Parent = $Site->getParent();
+$parentType = $Parent->getAttribute('type');
 
-if ( $parentType == 'quiqqer/faq:types/list' ||
-     $parentType == 'quiqqer/faq:types/category' )
-{
+if ($parentType == 'quiqqer/faq:types/list'
+    || $parentType == 'quiqqer/faq:types/category'
+) {
     QUI::getRewrite()->showErrorHeader(
         303,
-        URL_DIR . $Parent->getUrlRewrited() .'#faq'. $Site->getId()
+        URL_DIR.$Parent->getUrlRewrited().'#faq'.$Site->getId()
     );
 
     exit;
